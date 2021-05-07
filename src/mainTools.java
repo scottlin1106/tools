02cb52a858f3046012c0a09f967e5a6c293c81d2
scott.lin <corum9r833@gmail.com>
2021-05-07 10:05:07 +0800

    add sudoStr chkBox Modify setStr Check rule

--- src/mainTools.java
+++ src/mainTools.java
@@ -1,3 +1,4 @@
+import javafx.scene.control.CheckBox;
 import net.sf.json.JSONObject;
 import utils.HttpClientUtil;
 
@@ -19,23 +20,19 @@ public final class mainTools {
 
     static String paymentStr = "";
     static String autoStr = "";
-    static String reStr = "";
+    static String sudoStr = "sudo -i \n";
     static String logStr = "tail -f /usr/local/tomcat702/logs/catalina.out";
     static String dateLogStr = logStr + "." + date + ".log";
     static String logSearchStr = "cat /usr/local/tomcat702/logs/catalina.out";
     static String SearchStr = "cat /usr/local/tomcat702/logs/catalina.out";
-    static String pstsStr = "\\cp -r /tmp/psts/ contact.properties payname.properties /home/www/webapps/nfwApp/paystat/WEB-INF/classes/ \n";
+    static String pstsStr = "\\cp -r /tmp/psts/ contact.properties payname.properties /home/www/webapps/nfwApp/paystat/WEB-INF/classes/";
     static String dfSuccess = "http://www.yhsld.com/autopay/jsp/pay8.jsp";
     static String dfFailure = "http://www.yhsld.com/autopay/jsp/pay11.jsp";
 
-    static boolean isWork = true;
-    static JTextArea inputTextArea;
-    static JTextArea convertTextArea;
-    static JTextArea keywordTextArea;
-    static JTextArea convertNameTextArea;
-    static JComboBox dateBox;
-    static JComboBox colBox;
-
+    static boolean isWork = true, isSudo = false;
+    static JTextArea inputTextArea, convertTextArea, keywordTextArea, convertNameTextArea;
+    static JComboBox dateBox, colBox;
+    static JCheckBox sudoCheckBox;
     static int textAreaSize = 16;
 
     public static void main(String[] args) throws Exception {
@@ -47,13 +44,22 @@ public final class mainTools {
             /* 畫面配置 */
             JLabel inputStr = new JLabel("目的資料夾名稱");
             inputStr.setBounds(10, -5, 300, 50);
+            frame.add(inputStr);
 
             inputTextArea = new JTextArea(200, 5);
             inputTextArea.setBounds(150, 10, 500, 100);
             inputTextArea.setFont(new Font("Monospaced", Font.PLAIN, textAreaSize));
-
-            frame.add(inputStr);
             frame.add(inputTextArea);
+
+            JLabel sudoStr = new JLabel("sudo權限");
+            sudoStr.setBounds(10, 40, 300, 50);
+            frame.add(sudoStr);
+
+            sudoCheckBox = new JCheckBox();
+            sudoCheckBox.setBounds(80, 50, 50, 30);
+            sudoCheckBox.setSelected(true);
+            frame.add(sudoCheckBox);
+
 
             JLabel convertStr = new JLabel("代付訂單資訊");
             convertStr.setBounds(10, 230, 300, 50);
@@ -144,14 +150,15 @@ public final class mainTools {
             frame.add(searchBtn);
 
             /* 監聽配置 */
-            SimpleListener ourListener = new SimpleListener();
-            paymentBtn.addActionListener(ourListener);
-            autopayBtn.addActionListener(ourListener);
-            uatLogBtn.addActionListener(ourListener);
-            searchBtn.addActionListener(ourListener);
-            dfSucBtn.addActionListener(ourListener);
-            dfFaiBtn.addActionListener(ourListener);
-            paymentSumBtn.addActionListener(ourListener);
+            SimpleListener simpleListener = new SimpleListener();
+            SimpleListener2 simpleListener2 = new SimpleListener2();
+            paymentBtn.addActionListener(simpleListener2);
+            autopayBtn.addActionListener(simpleListener2);
+            uatLogBtn.addActionListener(simpleListener);
+            searchBtn.addActionListener(simpleListener);
+            dfSucBtn.addActionListener(simpleListener);
+            dfFaiBtn.addActionListener(simpleListener);
+            paymentSumBtn.addActionListener(simpleListener);
 
             frame.setLayout(null);
             frame.setVisible(true);
@@ -169,143 +176,164 @@ public final class mainTools {
         @Override
         public void actionPerformed(ActionEvent e) {
             String buttonName = e.getActionCommand();
-            System.out.println("[buttonName] : " + buttonName);
-            if (buttonName.equals("開始轉換")) {
-                setStr();
-            } else if (buttonName.equals("支付路徑")) {
-                if ("".equalsIgnoreCase(paymentStr)) {
-                    setStr();
-                }
-                copyStr(paymentStr);
-            } else if (buttonName.equals("代付路徑")) {
-                if ("".equalsIgnoreCase(autoStr)) {
-                    setStr();
-                }
-                copyStr(autoStr);
-            } else if (buttonName.equals("支付統計")) {
-                copyStr(pstsStr);
+            isSudo = sudoCheckBox.isSelected();
+            System.out.println("isSudo = " + isSudo);
+
+            if (buttonName.equals("支付統計")) {
+                copyStr(pstsStr, isSudo);
             } else if (buttonName.equals("UAT LOG")) {
-                copyStr(logStr);
+                copyStr(logStr, isSudo);
             } else if (buttonName.equals("PRD LOG")) {
-                copyStr(dateLogStr);
-            } else if (buttonName.equals("更改成功狀態")) {
-                String bodyStr = HttpClientUtil.formPostSkipSSL(dfSuccess, dfOutStr(convertTextArea.getText(), convertNameTextArea.getText()));
-                JSONObject jsObj = JSONObject.fromObject(bodyStr);
-                if ("1".equalsIgnoreCase(jsObj.optString("result"))) {
-                    JOptionPane.showMessageDialog(frame, "代付状态变更成功，请客户刷新页面查看", "请求返回", JOptionPane.INFORMATION_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(frame, bodyStr, "请检查报文内容", JOptionPane.INFORMATION_MESSAGE);
-                }
-
-            } else if (buttonName.equals("更改失敗狀態")) {
-                String bodyStr = HttpClientUtil.formPostSkipSSL(dfFailure, dfOutStr(convertTextArea.getText(), convertNameTextArea.getText()));
-                JSONObject jsObj = JSONObject.fromObject(bodyStr);
-                if ("0".equalsIgnoreCase(jsObj.optString("result"))) {
-                    JOptionPane.showMessageDialog(frame, jsObj.optJSONObject("error").optString("message"), "请求返回", JOptionPane.INFORMATION_MESSAGE);
-                } else {
-                    JOptionPane.showMessageDialog(frame, bodyStr, "请检查报文内容", JOptionPane.INFORMATION_MESSAGE);
-                }
-            } else if (buttonName.equals("LOG查询指令")) {
-                setLogStr((String) dateBox.getSelectedItem(), keywordTextArea.getText(), (String) colBox.getSelectedItem());
-                if(isWork) {
-                    copyStr(logSearchStr);
-                }
-
-            } else {
-                JOptionPane.showMessageDialog(frame, "Unknown event");
+                copyStr(dateLogStr, isSudo);
             }
+
+            try {
+                if (buttonName.equals("更改成功狀態")) {
+                    String bodyStr = HttpClientUtil.formPostSkipSSL(dfSuccess, dfOutStr(convertTextArea.getText(), convertNameTextArea.getText()));
+                    System.out.println("bodyStr = " + bodyStr);
+                    JSONObject jsObj = JSONObject.fromObject(bodyStr);
+                    if ("1".equalsIgnoreCase(jsObj.optString("result"))) {
+                        JOptionPane.showMessageDialog(frame, "代付状态变更成功，请客户刷新页面查看", "请求返回", JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(frame, jsObj.optJSONObject("error").optString("reason"), jsObj.optJSONObject("error").optString("message"), JOptionPane.INFORMATION_MESSAGE);
+                    }
+
+                } else if (buttonName.equals("更改失敗狀態")) {
+                    String bodyStr = HttpClientUtil.formPostSkipSSL(dfFailure, dfOutStr(convertTextArea.getText(), convertNameTextArea.getText()));
+                    JSONObject jsObj = JSONObject.fromObject(bodyStr);
+                    if ("0".equalsIgnoreCase(jsObj.optString("result"))) {
+                        JOptionPane.showMessageDialog(frame, jsObj.optJSONObject("error").optString("message"), "请求返回", JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(frame, bodyStr, "请检查报文内容", JOptionPane.INFORMATION_MESSAGE);
+                    }
+                } else if (buttonName.equals("LOG查询指令")) {
+                    setLogStr((String) dateBox.getSelectedItem(), keywordTextArea.getText(), (String) colBox.getSelectedItem());
+                    if (isWork) {
+                        copyStr(logSearchStr, isSudo);
+                    }
+                }
+            } catch (Exception e2) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+
+    private static class SimpleListener2 implements ActionListener {
+        /**
+         * 利用该内部类来监听所有事件源产生的事件
+         * 便于处理事件代码模块化
+         */
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String buttonName = e.getActionCommand();
+            isSudo = sudoCheckBox.isSelected();
+            System.out.println("isSudo = " + isSudo);
+            String inStr = inputTextArea.getText();
+            if (inStr != null && !"null".equalsIgnoreCase(inStr) && !"".equals(inStr)) {
+                paymentStr = "\\cp -r /tmp/payment/" + inStr + " /home/www/webapps/nfwApp/bg-payment/jsp \n";
+                autoStr = "\\cp -r /tmp/autopay/" + inStr + " /home/www/webapps/nfwApp/bg-autopay/jsp \n";
+
+                if (buttonName.equals("支付路徑")) {
+                    copyStr(paymentStr, isSudo);
+                } else if (buttonName.equals("代付路徑")) {
+                    copyStr(autoStr, isSudo);
+                }
+            } else {
+                JOptionPane.showMessageDialog(frame, "請輸入資料夾名稱", "提示", JOptionPane.WARNING_MESSAGE);
+            }
+
         }
     }
 
     public static Map<String, Object> dfOutStr(String inStr, String dfName) {
         Map<String, Object> postMap = new HashMap<String, Object>();
-        System.out.println("inStr = " + inStr);
-        System.out.println("dfName = " + dfName);
         try {
-            if ("".equalsIgnoreCase(inStr)) {
-                JOptionPane.showMessageDialog(frame, "转换内容不得为空", "错误", JOptionPane.ERROR_MESSAGE);
-            } else if ("".equalsIgnoreCase(dfName)) {
-                JOptionPane.showMessageDialog(frame, "代付名稱不得为空", "错误", JOptionPane.ERROR_MESSAGE);
-            } else {
-                List<String> list = new ArrayList();
-                list.add("sn");
-                list.add("payId");
-                list.add("withdrawId");
-                list.add("realAmount");
-                list.add("operatorId");
-                list.add("digest");
-                list.add("bankName");
-                list.add("orderNo");
-
+            if (inStr.contains(", ") && inStr.contains("=")) {
                 Map<String, Object> extMap = toolUtil.result(inStr, ", ", "=");
-                System.out.println("extMap = " + extMap);
-                if (extMap.get("OrderAmount") != null && !"null".equalsIgnoreCase((String) extMap.get("OrderAmount")) && !"".equals(extMap.get("OrderAmount"))) {
-                    extMap.put("realAmount", extMap.get("OrderAmount"));
-                    extMap.remove("OrderAmount");
+                if (inStr.contains("ProviderCode")) {
+                    String ProviderCode = (String) extMap.get("ProviderCode");
+                    dfName = ProviderCode.replaceAll(".sign", "");
+                    postMap.put("payname", dfName);
                 }
+                if ("".equalsIgnoreCase(inStr)) {
+                    JOptionPane.showMessageDialog(frame, "转换内容不得为空", "错误", JOptionPane.ERROR_MESSAGE);
+                } else if ("".equalsIgnoreCase(dfName)) {
+                    JOptionPane.showMessageDialog(frame, "代付名稱不得为空", "错误", JOptionPane.ERROR_MESSAGE);
+                } else {
+                    List<String> list = new ArrayList();
+                    list.add("sn");
+                    list.add("payId");
+                    list.add("withdrawId");
+                    list.add("realAmount");
+                    list.add("operatorId");
+                    list.add("digest");
+                    list.add("bankName");
+                    list.add("orderNo");
 
-                List<String> keys = new ArrayList<String>(extMap.keySet());
-                for (String str : list) {
-                    for (String extStr : keys) {
-                        if (str.equalsIgnoreCase(extStr)) {
-                            postMap.put(str, extMap.get(extStr));
+                    System.out.println("extMap = " + extMap);
+                    if (extMap.get("OrderAmount") != null && !"null".equalsIgnoreCase((String) extMap.get("OrderAmount")) && !"".equals(extMap.get("OrderAmount"))) {
+                        extMap.put("realAmount", extMap.get("OrderAmount"));
+                        extMap.remove("OrderAmount");
+                    }
+
+                    List<String> keys = new ArrayList<String>(extMap.keySet());
+                    for (String str : list) {
+                        for (String extStr : keys) {
+                            if (str.equalsIgnoreCase(extStr)) {
+                                postMap.put(str, extMap.get(extStr));
+                            }
                         }
                     }
                 }
-                postMap.put("payname", dfName);
+
+            } else {
+                throw new Exception();
             }
 
             System.out.println("[postMap] : " + postMap);
         } catch (Exception e) {
+            e.printStackTrace();
             JOptionPane.showMessageDialog(frame, "轉換內容請複製commpay字串", "请检查轉換内容", JOptionPane.INFORMATION_MESSAGE);
         }
         return postMap;
     }
 
-
-    public static void setStr() {
-        StringBuffer reStrbuf = new StringBuffer();
-        String inStr = inputTextArea.getText();
-        if (inStr != null && !"null".equalsIgnoreCase(inStr) && !"".equals(inStr)) {
-            paymentStr = "\\cp -r /tmp/payment/" + inStr + " /home/www/webapps/nfwApp/bg-payment/jsp \n";
-            autoStr = "\\cp -r /tmp/autopay/" + inStr + " /home/www/webapps/nfwApp/bg-autopay/jsp \n";
-            reStrbuf.append(
-                    "支付路徑 : \n" + paymentStr + "\n" +
-                            "代付路徑 : \n" + autoStr + "\n");
-            reStr = reStrbuf.toString();
-        } else {
-            JOptionPane.showMessageDialog(frame, "請輸入資料夾名稱", "提示", JOptionPane.WARNING_MESSAGE);
-
-        }
-    }
-
     public static void setLogStr(String date, String keyword, String col) {
         isWork = true;
-        String year = "20" + keyword.substring(0, 2);
-        String month = keyword.substring(2, 4);
-        String day = keyword.substring(4, 6);
-        String vDate = year + "-" + month + "-" + day;
-        System.out.println("vDate = " + vDate);
         String lsStr = SearchStr + "." + date + ".log";
-            if (!"".equalsIgnoreCase(keyword)) {
-                    if (toolUtil.validateDate(vDate)) {
-                        lsStr = SearchStr + "." + year + "-" + month + "-" + day + ".log | grep -C " + col + " " + keyword;
+        boolean isapn = false;
+        if (!(null == keyword || "".equalsIgnoreCase(keyword) || "null".equalsIgnoreCase(keyword))) {
+            if (keyword.length() > 7) {
+                String year = "20" + keyword.substring(0, 2);
+                String month = keyword.substring(2, 4);
+                String day = keyword.substring(4, 6);
+                String vDate = year + "-" + month + "-" + day;
+
+                if (toolUtil.validateDate(vDate)) {
+                    isapn = true;
+                    lsStr = SearchStr + "." + year + "-" + month + "-" + day + ".log | grep -C " + col + " " + keyword;
                 } else if (!toolUtil.validateDate(vDate)) {
-                        System.out.println("toolUtil.validateDate(vDate) = " + toolUtil.validateDate(vDate));
                     JOptionPane.showMessageDialog(frame, "查找日期超出LOG存檔期限(七天)", "提示", JOptionPane.WARNING_MESSAGE);
-                        isWork = false;
-                    }
-            } else {
-                lsStr += " | grep -C " + col + " " + keyword;
+                    isWork = false;
+                }
             }
-            logSearchStr = lsStr;
+            if(!isapn)
+            lsStr += " | grep -C " + col + " " + keyword;
+        }
+        logSearchStr = lsStr;
 
     }
 
-    public static void copyStr(String str) {
+    public static void copyStr(String str, boolean chkSudo) {
+        if (chkSudo)
+            str = sudoStr + str;
         JOptionPane.showMessageDialog(frame, str, "複製內容", JOptionPane.INFORMATION_MESSAGE);
         StringSelection selection = new StringSelection(str);
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         clipboard.setContents(selection, selection);
+    }
+
+    public static void copyStr(String str) {
+        copyStr(str, false);
     }
 }
